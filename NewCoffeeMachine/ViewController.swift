c12d402d3465ec6f267960a7d133939a31ba294b
Lynnyk <lynnykira@ukr.net>
2020-01-22 17:33:12 +0200

    Initial Commit

---
+++ NewCoffeeMachine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewCoffeeMachine
+//
+//  Created by Ира Линник on 22.01.2020.
+//  Copyright © 2020 Ира Линник. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
