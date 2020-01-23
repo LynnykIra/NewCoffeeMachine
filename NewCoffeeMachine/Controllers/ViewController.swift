2db803b428a74133e3937b04706f45d4fe005636
Lynnyk <lynnykira@ukr.net>
2020-01-23 19:08:12 +0200

    Add on button and welcome label on main screen

--- NewCoffeeMachine/Controllers/ViewController.swift
+++ NewCoffeeMachine/Controllers/ViewController.swift
@@ -9,15 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var backgroundMainStoryboard: UIImageView!
+    @IBOutlet weak var welcomeLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         addBackgroundImage()
     }
     func addBackgroundImage() {
-    backgroundMainStoryboard.image = UIImage (named: "coffee")
+        backgroundMainStoryboard.image = UIImage (named: "coffee")
+    }
+    @IBAction func onButton(_ sender: UIButton) {
+        
+        let automaticCoffeeMachine = CoffeeMachine.init(isAutomatic: true, materialOfCase: "metal")
+        welcomeLabel.text = "Hello, I'm your new coffee machine =) Would you like some coffee?"
+        
     }
 }
 
