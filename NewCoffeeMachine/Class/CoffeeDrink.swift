ee2e2739236d7c2f207097d35f78c7cfb4874781
Lynnyk <lynnykira@ukr.net>
2020-01-30 19:22:35 +0200

    Add make coffee drinks functions

--- NewCoffeeMachine/Class/CoffeeDrink.swift
+++ NewCoffeeMachine/Class/CoffeeDrink.swift
@@ -13,9 +13,52 @@ class CoffeDrink {
         return "Here is your drink, enjoy =)"
     }
     var coffee: Int
-    var milk: Int?
-    var cream: Int?
-    init (coffee: Int) {
+    var milk: Int
+    var cream: Int
+    var smallCupsUsed: Int
+    var mediumCupsUsed: Int
+    var largeCupsUsed: Int
+    init (coffee: Int, milk: Int, cream: Int, smallCupsUsed: Int, mediumCupsUsed: Int, largeCupsUsed: Int) {
         self.coffee = coffee
+        self.milk = milk
+        self.cream = cream
+        self.smallCupsUsed = smallCupsUsed
+        self.mediumCupsUsed = mediumCupsUsed
+        self.largeCupsUsed = largeCupsUsed
     }
+    let automaticCoffeMachine = CoffeeMachine.init(isAutomatic: true, materialOfCase: "metal")
+    
+    func makeEspresso() {
+        automaticCoffeMachine.takeAwayCoffee()
+        automaticCoffeMachine.takeAwaySmallCup()
+    }
+    
+    func makeDopio() {
+        automaticCoffeMachine.takeAwayCoffee()
+        automaticCoffeMachine.takeAwayCoffee()
+        automaticCoffeMachine.takeAwayMediumCup()
+    }
+    
+    func makeCuppuchino() {
+        automaticCoffeMachine.takeAwayCoffee()
+        for _ in 0..<3 { automaticCoffeMachine.takeAwayMilk()
+        }
+        automaticCoffeMachine.takeAwayLargeCup()
+    }
+    
+    func makeVienneseCoffee() {
+        automaticCoffeMachine.takeAwayCoffee()
+        automaticCoffeMachine.takeAwayCream()
+        automaticCoffeMachine.takeAwayCream()
+        automaticCoffeMachine.takeAwayMediumCup()
+    }
+    
+    func makelatteMacchiato() {
+        automaticCoffeMachine.takeAwayCoffee()
+        automaticCoffeMachine.takeAwayMilk()
+        automaticCoffeMachine.takeAwayMilk()
+        automaticCoffeMachine.takeAwayCream()
+        automaticCoffeMachine.takeAwayLargeCup()
+    }
+    
 }
