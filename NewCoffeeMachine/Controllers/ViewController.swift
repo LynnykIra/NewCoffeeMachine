cb3b6973abe39e867b2fd58388818f1b2e528f6f
Lynnyk <lynnykira@ukr.net>
2020-02-01 18:59:13 +0200

    Overwrites the functions of buttons for creating drinks

--- NewCoffeeMachine/Controllers/ViewController.swift
+++ NewCoffeeMachine/Controllers/ViewController.swift
@@ -16,8 +16,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         addBackgroundImage()
+        
     }
     let automaticCoffeeMachine = CoffeeMachine.init(isAutomatic: true, materialOfCase: "metal")
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let serviceVc = segue.destination as? ServiceViewController {
+            serviceVc.automaticCoffeeMachine = automaticCoffeeMachine
+        }
+    }
+    
     
     func addBackgroundImage() {
         backgroundMainStoryboard.image = UIImage (named: "coffee")
@@ -25,68 +33,54 @@ class ViewController: UIViewController {
     
     @IBAction func makeLatteMacchiatoButton(_ sender: UIButton) {
         let latteMacchiato = CoffeDrink.init(coffee: 10, milk: 20, cream: 10, smallCupsUsed: 0, mediumCupsUsed: 0, largeCupsUsed: 1)
-        if automaticCoffeeMachine.coffee < latteMacchiato.coffee {
-            welcomeLabel.text = "Please, add coffee"
-        } else if automaticCoffeeMachine.milk < latteMacchiato.milk {
-            welcomeLabel.text = "Please, add milk"
-        } else if automaticCoffeeMachine.cream < latteMacchiato.cream {
-            welcomeLabel.text = "Please, add cream"
-        } else if automaticCoffeeMachine.largeCups < latteMacchiato.largeCupsUsed {
-            welcomeLabel.text = "Please, add large cup"
-        } else if automaticCoffeeMachine.coffee > latteMacchiato.coffee && automaticCoffeeMachine.milk >  latteMacchiato.milk && automaticCoffeeMachine.cream > latteMacchiato.cream && automaticCoffeeMachine.largeCups > latteMacchiato.largeCupsUsed {
-            latteMacchiato.makelatteMacchiato()
+        welcomeLabel.text = automaticCoffeeMachine.checkResourses()
+        if welcomeLabel.text == "Coffee machine is ready" {
+            automaticCoffeeMachine.coffee -= latteMacchiato.coffee
+            automaticCoffeeMachine.milk -= latteMacchiato.milk
+            automaticCoffeeMachine.cream -= latteMacchiato.cream
+            automaticCoffeeMachine.largeCups -= latteMacchiato.largeCupsUsed
             welcomeLabel.text = latteMacchiato.description
         }
     }
     
     @IBAction func makeCuppuchinoButton(_ sender: UIButton) {
         let cuppuchino = CoffeDrink.init(coffee: 10, milk: 30, cream: 0, smallCupsUsed: 0, mediumCupsUsed: 0, largeCupsUsed: 1)
-        if automaticCoffeeMachine.coffee < cuppuchino.coffee {
-            welcomeLabel.text = "Please, add coffee"
-        } else if automaticCoffeeMachine.milk < cuppuchino.milk {
-            welcomeLabel.text = "Please, add milk"
-        } else if automaticCoffeeMachine.largeCups < cuppuchino.largeCupsUsed {
-            welcomeLabel.text = "Please, add large cup"
-        } else if automaticCoffeeMachine.coffee > cuppuchino.coffee && automaticCoffeeMachine.milk > cuppuchino.milk && automaticCoffeeMachine.largeCups > cuppuchino.largeCupsUsed {
-            cuppuchino.makeCuppuchino()
+        welcomeLabel.text = automaticCoffeeMachine.checkResourses()
+        if welcomeLabel.text == "Coffee machine is ready" {
+            automaticCoffeeMachine.coffee -= cuppuchino.coffee
+            automaticCoffeeMachine.milk -= cuppuchino.milk
+            automaticCoffeeMachine.largeCups -= cuppuchino.largeCupsUsed
             welcomeLabel.text = cuppuchino.description
         }
     }
     
     @IBAction func makeVienneseCoffee(_ sender: UIButton) {
         let vienneseCoffee = CoffeDrink.init(coffee: 10, milk: 0, cream: 20, smallCupsUsed: 0, mediumCupsUsed: 1, largeCupsUsed: 0)
-        if automaticCoffeeMachine.coffee < vienneseCoffee.coffee {
-            welcomeLabel.text = "Please, add coffee"
-        } else if automaticCoffeeMachine.mediumCups < vienneseCoffee.mediumCupsUsed {
-            welcomeLabel.text = "Please, add medium cup"
-        } else if automaticCoffeeMachine.cream < vienneseCoffee.cream {
-            welcomeLabel.text = "Please, add cream"
-        } else if automaticCoffeeMachine.coffee > vienneseCoffee.coffee && automaticCoffeeMachine.cream > vienneseCoffee.cream && automaticCoffeeMachine.mediumCups > vienneseCoffee.mediumCupsUsed {
-            vienneseCoffee.makeVienneseCoffee()
+        welcomeLabel.text = automaticCoffeeMachine.checkResourses()
+        if welcomeLabel.text == "Coffee machine is ready" {
+            automaticCoffeeMachine.coffee -= vienneseCoffee.coffee
+            automaticCoffeeMachine.cream -= vienneseCoffee.cream
+            automaticCoffeeMachine.mediumCups -= vienneseCoffee.mediumCupsUsed
             welcomeLabel.text = vienneseCoffee.description
         }
     }
     
     @IBAction func makeDopioButton(_ sender: UIButton) {
         let dopio = CoffeDrink.init(coffee: 20, milk: 0, cream: 0, smallCupsUsed: 0, mediumCupsUsed: 1, largeCupsUsed: 0)
-        if automaticCoffeeMachine.mediumCups < dopio.mediumCupsUsed {
-            welcomeLabel.text = "Please, add medium cup"
-        }   else if automaticCoffeeMachine.coffee < dopio.coffee {
-            welcomeLabel.text = "Please, add coffee"
-        } else if automaticCoffeeMachine.mediumCups > dopio.mediumCupsUsed && automaticCoffeeMachine.coffee > dopio.coffee {
-            dopio.makeDopio()
+        welcomeLabel.text = automaticCoffeeMachine.checkResourses()
+        if welcomeLabel.text == "Coffee machine is ready" {
+            automaticCoffeeMachine.coffee -= dopio.coffee
+            automaticCoffeeMachine.mediumCups -= dopio.mediumCupsUsed
             welcomeLabel.text = dopio.description
         }
     }
     
     @IBAction func makeEspressoButton(_ sender: UIButton) {
         let espresso = CoffeDrink.init(coffee: 10, milk: 0, cream: 0, smallCupsUsed: 1, mediumCupsUsed: 0, largeCupsUsed: 0)
-        if automaticCoffeeMachine.coffee < espresso.coffee {
-            welcomeLabel.text = "Please, add coffee"
-        } else if automaticCoffeeMachine.smallCups < espresso.smallCupsUsed {
-            welcomeLabel.text = "Please, add small cup"
-        } else if automaticCoffeeMachine.coffee > espresso.coffee && automaticCoffeeMachine.smallCups > espresso.smallCupsUsed {
-            espresso.makeEspresso()
+        welcomeLabel.text = automaticCoffeeMachine.checkResourses()
+        if welcomeLabel.text == "Coffee machine is ready" {
+            automaticCoffeeMachine.coffee -= espresso.coffee
+            automaticCoffeeMachine.smallCups -= espresso.smallCupsUsed
             welcomeLabel.text = espresso.description
         }
     }
@@ -96,4 +90,3 @@ class ViewController: UIViewController {
     }
     
 }
-
